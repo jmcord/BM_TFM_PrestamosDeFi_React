6e98f550a410812f3357600c33fc83b9f0f58edd
jmcord <jcorderodz@hotmail.com>
2024-02-11 17:37:28 +0100

    Owner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import {Header, Footer} from '../components'
+import {Header, Footer, Owner} from '../components'
 
 export default function Home() {
   return (
     <section className="flex flex-col place-items-center gap-4 py-20 px-20">
       <h1>Prestamos DeFi</h1>
       <Header />
+      <Owner />
       <Footer />
     </section>
   )

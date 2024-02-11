87530880545b608211fc5df124cce0fa3d5dd243
jmcord <jcorderodz@hotmail.com>
2024-02-11 12:20:27 +0100

    wagmi2

--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -1,16 +1,41 @@
-import { WagmiConfig, createConfig } from "wagmi";
-import { ConnectKitProvider, ConnectKitButton, getDefaultConfig } from "connectkit";
-import { sepolia } from 'wagmi/chains'
+import { WagmiProvider, createConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
-export const config = createConfig(
+const config = createConfig(
   getDefaultConfig({
-    // Required API Keys
-    alchemyId: import.meta.env.VITE_ALCHEMY_ID, // or infuraId
-    walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
+    // Your dApps chains
+    chains: [sepolia],
+    transports: {
+      // RPC URL for each chain
+      [mainnet.id]: http(
+        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+      ),
+    },
 
-    // Required
-    appName: "Prestamos DeFi Dapp",
-    chains: [sepolia]
+    // Required API Keys
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+
+    // Required App Info
+    appName: "Your App Name",
+
+    // Optional App Info
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
   }),
 );
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
 

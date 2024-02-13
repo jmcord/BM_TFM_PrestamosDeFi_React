63bd9959e993fcfe59b8de19087f3aefff1b2e5c
jmcord <jcorderodz@hotmail.com>
2024-02-13 13:30:22 +0100

    c

--- src/config/wagmi.jsx
+++ src/config/wagmi.jsx
@@ -1,33 +1,19 @@
-import { WagmiProvider, createConfig } from "wagmi";
+import { createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { getDefaultConfig } from "connectkit";
 
 export const config = createConfig(
   getDefaultConfig({
     // Your dApps chains
     chains: [sepolia],
-  
+
 
     // Required API Keys
-    walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
     alchemyId: import.meta.env.VITE_ALCHEMY_ID,
+    walletConnectProjectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
     // Required App Info
     appName: "Your App Name",
 
-
+ 
   }),
 );
-
-const queryClient = new QueryClient();
-
-export const Web3Provider = ({ children }) => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <ConnectKitProvider>{children}</ConnectKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
-};
-
